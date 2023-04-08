dc18aa21c4cde1d94bdd106f9e95de74bb2793a9
Basil Tatarnikov <tatvasily@gmail.com>
2023-04-09 03:21:09 +0800

    fix: потребовался метод для изменения вектора мнгновенной скорости

--- app/Move/Movable.php
+++ app/Move/Movable.php
@@ -7,6 +7,7 @@ use App\Vector;
 interface Movable
 {
     public function getVelocity(): Vector;
+    public function setVelocity(Vector $vector): void;
     public function getPosition(): Vector;
     public function setPosition(Vector $vector): void;
 }
