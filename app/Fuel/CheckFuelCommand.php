c599a738dc0c679fd6b079e0f00a0584dbb4684a
Basil Tatarnikov <tatvasily@gmail.com>
2023-04-08 23:12:32 +0800

    feat: добавлена команда проверки топлива

---
+++ app/Fuel/CheckFuelCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Fuel;
+
+use App\Exceptions\CommandException;
+use App\Fuel\Fuelable;
+use App\Interfaces\Command;
+use App\Vector;
+
+class CheckFuelCommand implements Command
+{
+    private Fuelable $target;
+
+    public function __construct(Fuelable $target)
+    {
+        $this->target = $target;
+    }
+
+    public function execute(): void
+    {
+        if ($this->target->getFuel() <= 0) {
+            throw new CommandException("Недостаточно остатка топлива!");
+        }
+    }
+}
