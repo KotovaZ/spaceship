424cba8810d6879e46ee85f7ab08b6fe342f9005
Basil Tatarnikov <tatvasily@gmail.com>
2023-04-11 11:35:54 +0800

    feat: Добавлен обработчик для повторного вызова команды и записи в лог

---
+++ app/Queue/RepeatAndLogExceptionHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Queue;
+
+use App\Interfaces\Command;
+use App\Log\LogExceptionCommand;
+use Exception;
+use Psr\Log\LoggerInterface;
+use Throwable;
+
+class RepeatAndLogExceptionHandler implements Command
+{
+    public function __construct(
+        private Throwable $exception,
+        private Command $targetCommand,
+        private LoggerInterface $logger
+    ) {
+    }
+
+    public function execute(): void
+    {
+        try {
+            (new RepeatCommand($this->targetCommand))->execute();
+        } catch (Throwable $e) {
+            (new LogExceptionCommand($this->exception, $this->logger))->execute();
+            throw $e;
+        }
+    }
+}
